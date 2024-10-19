fc9868ac143b1af51f757710d0d00ca2886305f2
ben <ben76020567@gmail.com>
2024-10-19 13:53:29 +0800

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,10 +23,10 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
+  -- spec = {
     -- import your plugins
-    { import = "plugins" },
-  },
+    -- { import = "plugins" },
+  -- },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
