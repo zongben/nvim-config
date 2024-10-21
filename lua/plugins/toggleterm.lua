77ef7f1d418477105481d6d2c4a96ceb0ffbd0b6
ben <ben76020567@gmail.com>
2024-10-21 17:47:04 +0800

    update

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,24 +1,24 @@
 return {
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  config = function()
-    require("toggleterm").setup({
-      open_mapping = [[<c-/>]],
-      size = 25,
-    })
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = function()
+		require("toggleterm").setup({
+			open_mapping = [[<c-/>]],
+			size = 22,
+		})
 
-    local Terminal = require("toggleterm.terminal").Terminal
-    local lazygit = Terminal:new({
-      cmd = "lazygit",
-      hidden = true,
-      direction = "float",
-      float_opts = {
-        border = "curved",
-      },
-    })
+		local Terminal = require("toggleterm.terminal").Terminal
+		local lazygit = Terminal:new({
+			cmd = "lazygit",
+			hidden = true,
+			direction = "float",
+			float_opts = {
+				border = "curved",
+			},
+		})
 
-    function Lazygit_toggle()
-      lazygit:toggle()
-    end
-  end,
+		function Lazygit_toggle()
+			lazygit:toggle()
+		end
+	end,
 }
