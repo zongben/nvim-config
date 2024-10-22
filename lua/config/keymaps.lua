58bb91e00ab9fd15477afa62128498e5ee81b761
ben <ben76020567@gmail.com>
2024-10-23 00:31:18 +0800

    update]

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,9 @@
 local function map(mode, lhs, rhs, opts)
-	local options = { noremap = true, silent = true }
-	if opts then
-		options = vim.tbl_extend("force", options, opts)
-	end
-	vim.keymap.set(mode, lhs, rhs, options)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.keymap.set(mode, lhs, rhs, options)
 end
 
 -- General
@@ -54,6 +54,10 @@ local builtin = require("telescope.builtin")
 map("n", "<leader>ff", builtin.find_files, {})
 map("n", "<leader>fg", builtin.live_grep, {})
 map("n", "<leader>fh", builtin.help_tags, {})
+map("n", "gd", builtin.lsp_definitions, {})
+map("n", "gI", builtin.lsp_implementations, {})
+map("n", "gr", builtin.lsp_references, {})
+map("n", "gt", builtin.lsp_type_definitions, {})
 map("n", "<leader>fn", "<cmd>Telescope notify<CR>", {})
 
 -- Copilot
@@ -64,11 +68,11 @@ map("n", "<leader>ms", "<cmd>Mason<CR>", {})
 
 -- LSP
 map("n", "K", vim.lsp.buf.hover, {})
-map("n", "gd", vim.lsp.buf.definition, {})
-map("n", "gD", vim.lsp.buf.declaration, {})
-map("n", "gI", vim.lsp.buf.implementation, {})
-map("n", "gr", vim.lsp.buf.references, {})
-map("n", "<C-k>", vim.lsp.buf.signature_help, {})
+-- map("n", "gd", vim.lsp.buf.definition, {})
+-- map("n", "gD", vim.lsp.buf.declaration, {})
+-- map("n", "gI", vim.lsp.buf.implementation, {})
+-- map("n", "gr", vim.lsp.buf.references, {})
+-- map("n", "<C-k>", vim.lsp.buf.signature_help, {})
 map("n", "<leader>rn", vim.lsp.buf.rename, {})
 map("n", "<leader>ca", vim.lsp.buf.code_action, {})
 map("n", "<leader>cf", vim.lsp.buf.format, {})
