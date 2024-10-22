74f58647b221d5cb5a1b67c1453151000a161e1e
Ben <ben76020567@gmail.com>
2024-10-22 14:04:23 +0800

    update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -30,6 +30,7 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+		lazy = false,
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
