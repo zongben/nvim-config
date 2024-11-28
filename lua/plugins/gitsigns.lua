47ab23caccf114a0c662fce0bd323ef9f013e4e7
Ben <ben76020567@gmail.com>
2024-11-28 17:47:56 +0800

    update

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -59,7 +59,24 @@ return {
           vim.keymap.set(mode, l, r, opts)
         end
 
+        map('n', ']c', function()
+          if vim.wo.diff then
+            vim.cmd.normal({ ']c', bang = true })
+          else
+            gitsigns.nav_hunk('next')
+          end
+        end)
+
+        map('n', '[c', function()
+          if vim.wo.diff then
+            vim.cmd.normal({ '[c', bang = true })
+          else
+            gitsigns.nav_hunk('prev')
+          end
+        end)
+
         map('n', '<leader>gb', gitsigns.toggle_current_line_blame)
+        map('n', '<leader>gd', gitsigns.diffthis)
       end
     })
   end,
