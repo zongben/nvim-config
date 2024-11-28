f762b7c62ae968b2062d4c17cf4c97083d41f65e
Ben <ben76020567@gmail.com>
2024-11-28 15:52:41 +0800

    update

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,17 +4,13 @@ return {
     config = function()
       require("mini.surround").setup({
         mappings = {
-          add = "<leader>sa",
-          delete = "<leader>sd",
           find = "",
           highlight = "",
-          replace = "<leader>sr",
           update_n_lines = "",
           suffix_last = "",
           suffix_next = "",
         },
         n_lines = 1,
-        search_method = "nearest",
       })
     end,
   },
