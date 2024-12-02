5fd8f8c29c71a85908dd185fb10c4fad15513144
Ben <ben76020567@gmail.com>
2024-12-02 17:25:00 +0800

    update

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,13 +1,23 @@
 return {
 	"folke/trouble.nvim",
 	opts = {
-		focus = true,
-		win = {
-			size = 0.25,
-		},
 		modes = {
+			symbols = {
+				win = {
+					size = 0.2,
+				},
+			},
 			diagnostics = {
+				win = {
+					size = 0.3,
+				},
 				keys = {
+					m = {
+						action = function(view)
+							view:filter({ buf = 0 }, { toggle = true })
+						end,
+						desc = "Toggle Current Buffer Filter",
+					},
 					s = {
 						action = function(view)
 							local f = view:get_filter("severity")
@@ -27,17 +37,22 @@ return {
 	keys = {
 		{
 			"<leader>xx",
-			"<cmd>Trouble diagnostics toggle<cr>",
+			"<cmd>Trouble diagnostics toggle focus=true<cr>",
 			desc = "Diagnostics (Trouble)",
 		},
 		{
+			"<leader>xl",
+			"<cmd>Trouble lsp toggle focus=true<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
 			"<leader>xs",
-			"<cmd>Trouble symbols toggle<cr>",
+			"<cmd>Trouble symbols toggle focus=true<cr>",
 			desc = "Symbols (Trouble)",
 		},
 		{
 			"<leader>xq",
-			"<cmd>Trouble qflist toggle<cr>",
+			"<cmd>Trouble qflist toggle focus=true<cr>",
 			desc = "Quickfix List (Trouble)",
 		},
 	},
