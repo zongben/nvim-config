83e964f4e2dc77d738a844c6ac8cd899690bbb29
ben <ben76020567@gmail.com>
2024-12-07 10:49:10 +0800

    update

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,5 +1,8 @@
 return {
   "ZongBen/project.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim"
+  },
   config = function()
     require("project_nvim").setup({})
     require("telescope").load_extension("projects")
