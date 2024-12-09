5b75d4dadd0ddd2088e21315f859e98e72f03879
ben <ben76020567@gmail.com>
2024-12-09 22:29:49 +0800

    update

--- lua/config/snippets.lua
+++ lua/config/snippets.lua
@@ -2,9 +2,33 @@ local ls = require("luasnip")
 
 local s = ls.snippet
 local i = ls.insert_node
-
+local t = ls.text_node
 local fmt = require("luasnip.extras.fmt").fmt
 
-ls.add_snippets("javascript", {
-  s("cl", fmt("console.log({})", { i(1) })),
+local to = s("to", {
+	i(1, "cond"),
+	t(" ? "),
+	i(2, "then"),
+	t(" : "),
+	i(3, "else"),
 })
+
+local csharp = {
+	to,
+}
+
+local js_ts = {
+	s("cl", fmt("console.log({})", { i(1) })),
+	s("map", {
+		t("map("),
+		i(1, "item"),
+		t(" => {"),
+		i(2),
+		t("})"),
+	}),
+  to
+}
+
+ls.add_snippets("javascript", js_ts)
+ls.add_snippets("typescript", js_ts)
+ls.add_snippets("csharp", csharp)
