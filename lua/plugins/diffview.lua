eee6c892142328105a638709a2cbced3df48e535
ben <ben76020567@gmail.com>
2024-12-13 11:36:00 +0800

    update

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -2,11 +2,16 @@ return {
   "sindrets/diffview.nvim",
   config = function()
     require("diffview").setup({
+      view = {
+        merge_tool = {
+          layout = "diff3_mixed",
+        }
+      },
       file_panel = {
         win_config = {
           position = "right",
         }
-      }
+      },
     })
   end
 }
