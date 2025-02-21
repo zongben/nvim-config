0c03d0cda8415399e3cef31b6dec46c85d3cb105
ben <ben76020567@gmail.com>
2025-02-21 21:51:57 +0800

    update

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -37,7 +37,6 @@ return {
         virt_text_priority = 100,
         use_focus = true,
       },
-      current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
       sign_priority = 6,
       update_debounce = 100,
       status_formatter = nil, -- Use default
