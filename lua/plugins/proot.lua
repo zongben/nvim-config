9b76f5c44b7ad9e52e4a1f488b6f0581e897278b
Ben <ben76020567@gmail.com>
2025-05-02 15:19:40 +0800

    update

--- lua/plugins/proot.lua
+++ lua/plugins/proot.lua
@@ -1,6 +1,13 @@
 return {
   "zongben/proot.nvim",
-  config = function ()
-    require("proot").setup()
-  end
+  config = function()
+    require("proot").setup({
+      events = {
+        entered = function()
+          vim.cmd("bufdo bd")
+          vim.cmd("LspRestart")
+        end,
+      },
+    })
+  end,
 }
