b04d11d24e4eec3ff044e8d90df8aa941bb0bf7e
ben <ben76020567@gmail.com>
2025-06-03 16:38:06 +0800

    update

--- lua/plugins/proot.lua
+++ lua/plugins/proot.lua
@@ -12,7 +12,6 @@ return {
           if require("toggleterm.terminal").get(1) then
             vim.cmd("TermExec cmd='exit'")
           end
-          vim.fn.chdir(path)
           vim.cmd("bufdo bd")
           vim.cmd("LspRestart")
         end,
