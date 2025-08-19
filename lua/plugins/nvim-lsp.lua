44f91befe6000154217f7ef752fb146622de87fa
ben <ben76020567@gmail.com>
2025-08-19 18:13:19 +0800

    upd

--- lua/plugins/nvim-lsp.lua
+++ lua/plugins/nvim-lsp.lua
@@ -66,48 +66,48 @@ return {
       -- "Hoffs/omnisharp-extended-lsp.nvim",
     },
     config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-
-      lspconfig.bashls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.tailwindcss.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.docker_compose_language_service.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.eslint.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.jsonls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.cssls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.clangd.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.svelte.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.prismals.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.sqls.setup({
-        capabilities = capabilities,
-      })
+      -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      -- local lspconfig = require("lspconfig")
+      --
+      -- lspconfig.bashls.setup({
+      --   capabilities = capabilities,
+      -- })
+      -- lspconfig.tailwindcss.setup({
+      --   capabilities = capabilities,
+      -- })
+      -- lspconfig.docker_compose_language_service.setup({
+      --   capabilities = capabilities,
+      -- })
+      -- lspconfig.gopls.setup({
+      --   capabilities = capabilities,
+      -- })
+      -- lspconfig.html.setup({
+      --   capabilities = capabilities,
+      -- })
+      -- lspconfig.eslint.setup({
+      --   capabilities = capabilities,
+      -- })
+      -- lspconfig.jsonls.setup({
+      --   capabilities = capabilities,
+      -- })
+      -- lspconfig.cssls.setup({
+      --   capabilities = capabilities,
+      -- })
+      -- lspconfig.ts_ls.setup({
+      --   capabilities = capabilities,
+      -- })
+      -- lspconfig.clangd.setup({
+      --   capabilities = capabilities,
+      -- })
+      -- lspconfig.svelte.setup({
+      --   capabilities = capabilities,
+      -- })
+      -- lspconfig.prismals.setup({
+      --   capabilities = capabilities,
+      -- })
+      -- lspconfig.sqls.setup({
+      --   capabilities = capabilities,
+      -- })
     end,
   },
 }
