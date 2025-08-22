b883b343d9cd0e99147a0efaec756f1dc69568df
ben <ben76020567@gmail.com>
2025-08-22 17:22:48 +0800

    upd

--- lua/plugins/proot.lua
+++ lua/plugins/proot.lua
@@ -17,7 +17,7 @@ return {
           local clients = vim.lsp.get_clients()
           for _, client in pairs(clients) do
             if client.name ~= "copilot" then
-              vim.cmd("LspRestart " .. client.name)
+              vim.lsp.stop_client(client)
             end
           end
         end,
