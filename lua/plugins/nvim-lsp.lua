545bc1dc283f7db5d9b3869701432e4418018f8f
ben <ben76020567@gmail.com>
2025-08-22 18:32:51 +0800

    upd

--- lua/plugins/nvim-lsp.lua
+++ lua/plugins/nvim-lsp.lua
@@ -12,7 +12,7 @@ return {
   },
   {
     "mason-org/mason.nvim",
-    opts = {}
+    opts = {},
   },
   {
     "williamboman/mason-lspconfig.nvim",
@@ -62,52 +62,6 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    dependencies = {
-      -- "Hoffs/omnisharp-extended-lsp.nvim",
-    },
-    config = function()
-      -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      -- local lspconfig = require("lspconfig")
-      --
-      -- lspconfig.bashls.setup({
-      --   capabilities = capabilities,
-      -- })
-      -- lspconfig.tailwindcss.setup({
-      --   capabilities = capabilities,
-      -- })
-      -- lspconfig.docker_compose_language_service.setup({
-      --   capabilities = capabilities,
-      -- })
-      -- lspconfig.gopls.setup({
-      --   capabilities = capabilities,
-      -- })
-      -- lspconfig.html.setup({
-      --   capabilities = capabilities,
-      -- })
-      -- lspconfig.eslint.setup({
-      --   capabilities = capabilities,
-      -- })
-      -- lspconfig.jsonls.setup({
-      --   capabilities = capabilities,
-      -- })
-      -- lspconfig.cssls.setup({
-      --   capabilities = capabilities,
-      -- })
-      -- lspconfig.ts_ls.setup({
-      --   capabilities = capabilities,
-      -- })
-      -- lspconfig.clangd.setup({
-      --   capabilities = capabilities,
-      -- })
-      -- lspconfig.svelte.setup({
-      --   capabilities = capabilities,
-      -- })
-      -- lspconfig.prismals.setup({
-      --   capabilities = capabilities,
-      -- })
-      -- lspconfig.sqls.setup({
-      --   capabilities = capabilities,
-      -- })
-    end,
+    config = function() end,
   },
 }
