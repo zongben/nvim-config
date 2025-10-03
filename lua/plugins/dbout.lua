b80cab3cff1de9ba9be8780bf77fbd8e404c1384
ben <ben76020567@gmail.com>
2025-10-04 00:42:09 +0800

    upd

--- lua/plugins/dbout.lua
+++ lua/plugins/dbout.lua
@@ -1,10 +1,6 @@
 return {
   "zongben/dbout.nvim",
   build = "npm install",
-  dependencies = {
-    "nvim-telescope/telescope.nvim",
-    "nvim-lua/plenary.nvim",
-  },
   config = function()
     require("dbout").setup({})
   end,
