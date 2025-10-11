0fa8c7834c13248305fca54ec6f1aca56170c80e
ben <ben76020567@gmail.com>
2025-10-11 15:50:49 +0800

    upd

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "echasnovski/mini.surround",
+    "nvim-mini/mini.surround",
     config = function()
       require("mini.surround").setup({
         mappings = {
@@ -15,21 +15,27 @@ return {
     end,
   },
   {
-    "echasnovski/mini.move",
+    "nvim-mini/mini.move",
     config = function()
       require("mini.move").setup()
     end,
   },
   {
-    "echasnovski/mini.ai",
+    "nvim-mini/mini.ai",
     config = function()
       require("mini.ai").setup()
     end,
   },
   {
-    "echasnovski/mini.pairs",
+    "nvim-mini/mini.pairs",
     config = function()
       require("mini.pairs").setup()
+    end,
+  },
+  {
+    "nvim-mini/mini.comment",
+    config = function()
+      require("mini.comment").setup()
     end,
   },
 }
