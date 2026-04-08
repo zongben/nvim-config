d6df35f73f255faa33961bc9c797dc2cc8c7fb0c
ben <ben76020567@gmail.com>
2026-04-09 00:54:18 +0800

    upd

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -16,6 +16,7 @@ return {
   config = function()
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
+    vim.opt.termguicolors = true
 
     local prev = { new_name = "", old_name = "" }
     vim.api.nvim_create_autocmd("User", {
@@ -33,6 +34,25 @@ return {
 
     require("nvim-tree").setup({
       sync_root_with_cwd = true,
+      on_attach = function(bufnr)
+        local api = require("nvim-tree.api")
+
+        api.map.on_attach.default(bufnr)
+
+        local function edit_or_open()
+          local node = api.tree.get_node_under_cursor()
+          ---@diagnostic disable-next-line: undefined-field, need-check-nil
+          if node.nodes ~= nil then
+            api.node.open.edit()
+          else
+            api.node.open.edit()
+            api.tree.close()
+          end
+        end
+
+        vim.keymap.set("n", "l", edit_or_open, { buffer = bufnr })
+        vim.keymap.set("n", "h", api.node.navigate.parent_close, { buffer = bufnr })
+      end,
       view = {
         side = "right",
         number = true,
@@ -42,6 +62,24 @@ return {
       actions = {
         open_file = {
           quit_on_open = true,
+        },
+      },
+      renderer = {
+        highlight_git = "all",
+        special_files = {},
+        icons = {
+          git_placement = "right_align",
+          glyphs = {
+            git = {
+              unstaged = "U",
+              staged = "A",
+              unmerged = "M",
+              renamed = "R",
+              untracked = "?",
+              deleted = "D",
+              ignored = "!",
+            },
+          },
         },
       },
     })
