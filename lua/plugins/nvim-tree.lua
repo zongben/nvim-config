57f31dfdec71992bc9204bdc4a6220bfc1b0d986
ben <ben76020567@gmail.com>
2026-04-09 16:27:53 +0800

    upd

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -32,8 +32,6 @@ return {
       end,
     })
 
-    local WIDTH_RATIO = 0.25
-    -- local HEIGHT_RATIO = 0.8
     require("nvim-tree").setup({
       sync_root_with_cwd = true,
       on_attach = function(bufnr)
@@ -59,31 +57,8 @@ return {
         number = true,
         relativenumber = true,
         side = "right",
-        -- float = {
-        --   enable = true,
-        --   open_win_config = function()
-        --     local screen_w = vim.opt.columns:get()
-        --     ---@diagnostic disable-next-line: undefined-field
-        --     local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
-        --     local window_w = screen_w * WIDTH_RATIO
-        --     local window_h = screen_h * HEIGHT_RATIO
-        --     local window_w_int = math.floor(window_w)
-        --     local window_h_int = math.floor(window_h)
-        --     local center_x = (screen_w - window_w) / 2
-        --     ---@diagnostic disable-next-line: undefined-field
-        --     local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
-        --     return {
-        --       border = "rounded",
-        --       relative = "editor",
-        --       row = center_y,
-        --       col = center_x,
-        --       width = window_w_int,
-        --       height = window_h_int,
-        --     }
-        --   end,
-        -- },
         width = function()
-          return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+          return math.floor(vim.opt.columns:get() * 0.25)
         end,
       },
       actions = {
