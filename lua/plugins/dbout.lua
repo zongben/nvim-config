a528376c460940140159089347f4274b16c3b6e3
ben <ben76020567@gmail.com>
2026-04-10 16:54:06 +0800

    upd

--- lua/plugins/dbout.lua
+++ lua/plugins/dbout.lua
@@ -2,12 +2,17 @@ return {
   "zongben/dbout.nvim",
   build = "npm install",
   lazy = "VeryLazy",
-  cmd = { "Dbout" },
   -- dir = "~/github/dbout.nvim",
+  keys = {
+    {
+      "<leader>db",
+      function()
+        vim.cmd("Dbout")
+      end,
+      mode = "n",
+    },
+  },
   config = function()
     require("dbout").setup({})
-
-    local map = require("utils").map
-    map("n", "<leader>db", "<cmd>Dbout<cr>", {})
   end,
 }
